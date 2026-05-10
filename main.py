7edb71cfcb92496e2f504d21f3a80f03488e6cd5
AndrewTechTips <andreww.steel69@gmail.com>
2026-05-10 11:48:51 +0300

    add SQLite database integration for event data retrieval

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import requests
 import selectorlib
 import smtplib, ssl
 import os
+import sqlite3
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
 }
+
+connection = sqlite3.connect("data.db")
 
 
 def scrape(url):
@@ -45,8 +48,18 @@ def store(extracted):
 
 
 def read(extracted):
-    with open("data.txt", "r") as file:
-        return file.read()
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date)
+    )
+
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
 
 
 if __name__ == "__main__":
@@ -54,9 +67,9 @@ if __name__ == "__main__":
     extracted = extract(scrapped)
 
     print(extracted)
-    content = read(extracted)
 
     if extracted != "No upcoming tours":
-        if extracted not in content:
+        row = read(extracted)
+        if not row:
             store(extracted)
             send_email(message="Hey, new event was found")
